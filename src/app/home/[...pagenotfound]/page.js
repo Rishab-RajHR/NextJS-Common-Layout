0979b5a0a4c565cac15ef46cb7c02b3ff1a2db9a
Rishab Raj <rr0948555@gmail.com>
2026-01-20 23:23:27 +0530

    Update page.js

--- src/app/home/[...pagenotfound]/page.js
+++ src/app/home/[...pagenotfound]/page.js
@@ -1,7 +1,7 @@
 const RouteBasedPageNot = () => {
     return (
        <>
-        <h2>404 Page | Page Not For Home Route</h2>
+        <h2>404 Page | Page Not For Home Routes</h2>
        </>
     );
 }
