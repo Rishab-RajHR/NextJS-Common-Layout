a16535de3bf14dbff1e302e9685a1dd12266a1af
Rishab Raj <rr0948555@gmail.com>
2026-02-03 13:02:34 +0530

    Variables

--- src/app/env-variables/page.js
+++ src/app/env-variables/page.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const EnvironmentVariables = () => {
-  console.log(process.env)
+  // console.log(process.env);
+  console.log(process.env.GOOGLEMAPSAPIKEY);
   return (
     <>
       <h2>Environment Variables in Next JS</h2>
-      {
+      {/* {
          process.env.NODE_ENV == 'development' ? <h3>development Mode</h3> : <h3>Production Mode</h3>
-      }
+      } */}
     </>
   )
 }
