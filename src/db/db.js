b88f84c6e8d6e9ad24bb9c1f3ab7f3f0c0c194a8
Rishab Raj <rr0948555@gmail.com>
2026-02-05 13:39:46 +0530

    API data

---
+++ src/db/db.js
@@ -0,0 +1,20 @@
+export const employee = [
+    {
+        employeeId : 1,
+        name: 'Alex',
+        salary: '70k',
+        email : 'alex12@gmail.com'
+    },
+    {
+        employeeId : 2,
+        name: 'Tovino',
+        salary: '60k',
+        email : 'tovino65@gmail.com'
+    },
+    {
+        employeeId : 1,
+        name: 'Basil',
+        salary: '80k',
+        email : 'basil8588@gmail.com'
+    },
+]
