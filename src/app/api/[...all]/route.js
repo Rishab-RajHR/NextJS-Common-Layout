106c28f385e0cfc79ecad43a376875bafef850e4
Rishab Raj <rr0948555@gmail.com>
2026-02-12 13:49:45 +0530

    Spread operator all

---
+++ src/app/api/[...all]/route.js
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server"
+
+
+export const GET = async (req, value) => {
+    // console.log(value.params.all)
+    const details = value.params.all
+    // return NextResponse.json({result: "Success"})
+    return NextResponse.json({result: details})
+}
