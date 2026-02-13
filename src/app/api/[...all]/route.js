3dfff1d8d3c4a8882ad6a3aa1ecdedfe55d9b21d
Rishab Raj <rr0948555@gmail.com>
2026-02-13 13:32:58 +0530

    MongoDB

--- src/app/api/[...all]/route.js
+++ src/app/api/[...all]/route.js
@@ -5,5 +5,5 @@ export const GET = async (req, value) => {
     // console.log(value.params.all)
     const details = value.params.all
     // return NextResponse.json({result: "Success"})
-    return NextResponse.json({result: details})
+    return NextResponse.json({result: details});
 }
